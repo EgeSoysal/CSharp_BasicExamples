874041ae46fe427a6e7f0843793cecd3397e4601
Ege Soysal <egesoysal0@gmail.com>
2025-11-16 12:47:11 +0300

    Add C# projects: String, int, double, char examp.
    This commit includes four basic C# example projects demonstrating:
    
    1. String variables and concatenation
    2. Integer variables and basic arithmetic operations
    3. Double variables and calculations with decimals
    4. Char variables and usage examples
    
    Each project contains code examples and console output demonstrating the respective concepts.

---
+++ IntDoubleCharString/IntDoubleCharString/Program.cs
@@ -0,0 +1,105 @@
+﻿
+#region C# String Variables and Car Information Example
+//string carName;
+//carName = "Car One";
+//Console.WriteLine(carName);
+//Console.WriteLine("***************************");
+//Console.WriteLine();
+//string carModel, carColor, carYear, carPrice;
+
+//carModel = "Model Y";
+//carColor = "Red";
+//carYear = "2025";
+//carPrice = "50000 $";
+//Console.WriteLine("********* Car Info *********");
+//Console.WriteLine();
+//Console.WriteLine("Car Model: " + carModel);
+//Console.WriteLine("Car Color: " + carColor);
+//Console.WriteLine("Car Year: " + carYear);
+//Console.WriteLine("Car Price: " + carPrice);
+//Console.WriteLine("****************************");
+#endregion
+
+#region C# Integer Variables and Arithmetic Operations Example
+//int websitePrice = 100;
+//int hostingPrice = 50;
+//int domainPrice = 20;
+//int maintenancePrice = 30;
+//int totalPrice;
+
+//Console.WriteLine("*** Website Service Price List ***");
+//Console.WriteLine();
+//Console.WriteLine("Website Development Price: " + websitePrice + " $");
+//Console.WriteLine("Hosting Price: " + hostingPrice + " $");
+//Console.WriteLine("Domain Price: " + domainPrice + " $");
+//Console.WriteLine("Maintenance Price: " + maintenancePrice + " $");
+//Console.WriteLine();
+//totalPrice = websitePrice + hostingPrice + domainPrice + maintenancePrice;
+//Console.WriteLine("******* Total Service Price *******");
+
+//Console.WriteLine("Total Price: " + totalPrice + " $");
+//Console.WriteLine("***********************************");   
+#endregion
+
+#region C# Double Variables and Price Calculation Example 
+//double number = 8.15;
+//Console.WriteLine(number);
+
+//Console.WriteLine("** Price List **\n");
+
+//// Prices per unit
+//double applePrice = 40.25;
+//double orangePrice = 31.65;
+//double bananaPrice = 78.56;
+//double tomatoPrice = 23.32;
+
+//Console.WriteLine($"Apple Price: ${applePrice}");
+//Console.WriteLine($"Orange Price: ${orangePrice}");
+//Console.WriteLine($"Banana Price: ${bananaPrice}");
+//Console.WriteLine($"Tomato Price: ${tomatoPrice}");
+
+//// Weights in kilograms
+//double appleGram = 1.354;
+//double orangeGram = 6.563;
+//double bananaGram = 2.630;
+//double tomatoGram = 0.353;
+
+//// Total prices
+//double appleTotalPrice = appleGram * applePrice;
+//double orangeTotalPrice = orangeGram * orangePrice;
+//double bananaTotalPrice = bananaGram * bananaPrice;
+//double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+//Console.WriteLine("\n*** Received Product Weights ***\n");
+//Console.WriteLine("-----------------------------------");
+//Console.WriteLine($"Total amount of Apples: ${appleTotalPrice:F2}");
+//Console.WriteLine($"Total amount of Oranges: ${orangeTotalPrice:F2}");
+//Console.WriteLine($"Total amount of Bananas: ${bananaTotalPrice:F2}");
+//Console.WriteLine($"Total amount of Tomatoes: ${tomatoTotalPrice:F2}");
+//Console.WriteLine("-----------------------------------\n");
+
+//Console.WriteLine("-----------------------------------");
+//Console.WriteLine($"Purchased Item: Apple - Unit Price: ${applePrice} - Weight: {appleGram}kg - Total Price: ${appleTotalPrice:F2}");
+//Console.WriteLine($"Purchased Item: Orange - Unit Price: ${orangePrice} - Weight: {orangeGram}kg - Total Price: ${orangeTotalPrice:F2}");
+//Console.WriteLine($"Purchased Item: Banana - Unit Price: ${bananaPrice} - Weight: {bananaGram}kg - Total Price: ${bananaTotalPrice:F2}");
+//Console.WriteLine($"Purchased Item: Tomato - Unit Price: ${tomatoPrice} - Weight: {tomatoGram}kg - Total Price: ${tomatoTotalPrice:F2}");
+//Console.WriteLine("-----------------------------------\n");
+
+//Console.WriteLine("-----------------------------------");
+//Console.WriteLine("*** SHOPPING TOTAL ***");
+//double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + bananaTotalPrice + tomatoTotalPrice;
+//Console.WriteLine($"Total Shopping Price: ${shoppingTotalPrice:F2}");
+//Console.WriteLine("-----------------------------------\n");
+#endregion
+
+#region C# Char Variable Example
+//char letter = 'A';
+//char digit = '7';
+//char symbol = '#';
+//char space = ' ';
+
+//Console.WriteLine("Letter: " + letter);   // Output: Letter: A
+//Console.WriteLine("Digit: " + digit);     // Output: Digit: 7
+//Console.WriteLine("Symbol: " + symbol);   // Output: Symbol: #
+//Console.WriteLine("Space: '" + space + "'"); // Output: Space: ' '
+#endregion
