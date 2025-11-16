5991b7c6053506d576fa4f7dc40ebe26cc1d56c3
Ege Soysal <egesoysal0@gmail.com>
2025-11-16 13:08:15 +0300

    Parse Examples: string, int, double, char
    This commit includes four small C# projects demonstrating the use of Parse for user input:
    
    1. String Example: Collect passenger information from the console.
    2. Int Example: Calculate total price in a shopping cart based on quantities.
    3. Double Example: Compute average of three exam scores.
    4. Char Example: Input and display gender selection.
    
    Each project shows how to read input from the user and convert it to the correct data type using Parse methods.

---
+++ Parse_Examples/Parse_Examples/Program.cs
@@ -0,0 +1,81 @@
+﻿
+#region String Example
+//Console.WriteLine("*** C# Passenger Information ***");
+
+//string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge;
+
+//Console.Write("Enter Passenger Name: ");
+//passengerName = Console.ReadLine();
+
+//Console.Write("Enter Passenger Surname: ");
+//passengerSurname = Console.ReadLine();
+
+//Console.Write("Enter Passenger District: ");
+//passengerDistrict = Console.ReadLine();
+
+//Console.Write("Enter Passenger City: ");
+//passengerCity = Console.ReadLine();
+
+//Console.Write("Enter Passenger Age: ");
+//passengerAge = Console.ReadLine();
+
+//Console.WriteLine("------------------");
+//Console.WriteLine($"Passenger: {passengerName} {passengerSurname}, {passengerDistrict}, {passengerCity}, Age: {passengerAge}");
+
+//Console.WriteLine("-------------------");
+#endregion  
+
+#region İnt & Parse Example 
+
+//Console.WriteLine("*** C# Shopping Cart ***");
+
+//int carPrice = 150000, computerPrice = 50000, chairPrice = 1500, tvPrice = 80000;
+
+//Console.Write("Enter number of Cars you want to buy: ");
+//int carCount = int.Parse(Console.ReadLine());
+
+//Console.Write("Enter number of Computers you want to buy: ");
+//int computerCount = int.Parse(Console.ReadLine());
+
+//Console.Write("Enter number of Chairs you want to buy: ");
+//int chairCount = int.Parse(Console.ReadLine());
+
+//Console.Write("Enter number of TVs you want to buy: ");
+//int tvCount = int.Parse(Console.ReadLine());
+
+//int totalPrice = carCount * carPrice + computerCount * computerPrice + chairCount * chairPrice + tvCount * tvPrice;
+
+//Console.WriteLine($"Total Amount to Pay: ${totalPrice}");
+
+//Console.WriteLine("------------------------------");
+#endregion
+
+#region Double & Parse Example
+
+//Console.WriteLine("*** C# Exam Average Calculator ***");
+
+//Console.Write("Enter First Exam Score: ");
+//double exam1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Enter Second Exam Score: ");
+//double exam2 = double.Parse(Console.ReadLine());
+
+//Console.Write("Enter Third Exam Score: ");
+//double exam3 = double.Parse(Console.ReadLine());
+
+//double average = (exam1 + exam2 + exam3) / 3;
+
+//Console.WriteLine($"\nYour Exam Average: {average:F2}");
+
+//Console.WriteLine("-----------------------------------------");
+#endregion
+
+#region Char & Parse Example    
+
+//Console.WriteLine("*** C# Gender Input ***\n");
+
+//Console.Write("Select Gender (M/F): ");
+//char gender = char.Parse(Console.ReadLine());
+
+//Console.WriteLine($"You selected: {gender}");
+#endregion
