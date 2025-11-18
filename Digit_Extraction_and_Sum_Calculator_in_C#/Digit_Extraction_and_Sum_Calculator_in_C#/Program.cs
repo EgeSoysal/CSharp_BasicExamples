c7761088ada4fd94d0c9972727c9c210b29e1139
Ege Soysal <egesoysal0@gmail.com>
2025-11-18 19:53:44 +0300

    Add three-digit number analyzer program
    This commit adds a C# console application that analyzes a three-digit number entered by the user.
    The program extracts the ones, tens, and hundreds digits, displays each value individually, and calculates the total sum of the digits.
    It also includes improved console formatting for a clearer and more user/friendly output.

---
+++ Digit_Extraction_and_Sum_Calculator_in_C#/Digit_Extraction_and_Sum_Calculator_in_C#/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("--------------------------------------");
+Console.WriteLine("     THREE/DIGIT NUMBER ANALYZER     ");
+Console.WriteLine("--------------------------------------");
+Console.Write("Enter a number: ");
+
+int number = int.Parse(Console.ReadLine());
+
+// Extract digits
+int ones = number % 10;
+int tens = (number % 100) / 10;
+int hundreds = number / 100;
+
+Console.WriteLine("DIGIT BREAKDOWN");
+Console.WriteLine($"Ones Digit     : {ones}");
+Console.WriteLine($"Tens Digit     : {tens}");
+Console.WriteLine($"Hundreds Digit : {hundreds}");
+
+int sum = ones + tens + hundreds;
+
+Console.WriteLine("RESULT");
+Console.WriteLine($"Sum of Digits  : {sum}");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Calculation Completed!");
+Console.WriteLine("--------------------------------------");
+
+
